9cd5165b4f7cd43910fc2f92ef2201d9605eb5d7
Yuval Wyborski <yvw.bor@gmail.com>
2015-03-09 10:46:35 +0200

    Use raycasting engine to calculate FOV

--- src/objects.py
+++ src/objects.py
@@ -1,3 +1,10 @@
+from collections import defaultdict
+
+try:
+    from . import raycast as rc
+except:
+    import raycast as rc
+
 class Object:
     def __init__(self, location, symbol, color, visible, game_map=None):
         self.location = location
@@ -17,10 +24,33 @@ class Object:
 
 
 class Player(Object):
+
+
     '''
         The playable player object
     '''
-    def __init__(self, location, symbol, color, game_map):
+    def __init__(self, location, symbol, color, game_map, **kwargs):
         super().__init__(location, symbol, color, True, game_map)
+        
+        self.explored = set()
+
+        self.fov = kwargs.get("fov", 100)
+
+        self.update_fov()
+
+
+    def move(self, velocity):
+        super().move(velocity)
+        
+        self.update_fov()
+
+    def update_fov(self):
+
+        self.explored.add(self.location)
+        visible = rc.cast_rays(self, self.map, self.fov)
+
+        self.visible = set(visible)
+        self.explored = self.explored.union(set(visible))
+
 
     
